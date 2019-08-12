e465b1c6b3d7dc44a74a0cc7932f0649f79bea65
kreuzerk <kevin.kreuzer90@icloud.com>
2019-08-12 14:06:55 +0200

    fix(howdy): use correct selector for howdy-time

--- projects/howdy/src/lib/time/howdy-time.component.ts
+++ projects/howdy/src/lib/time/howdy-time.component.ts
@@ -2,7 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import moment from 'moment';
 
 @Component({
-  selector: 'howdy-weather',
+  selector: 'howdy-time',
   template: `<h1>Hello today is the {{time}}</h1>`
 })
 export class HowdyTimeComponent implements OnInit {
--- projects/howdy/src/lib/weather/howdy-weather.component.ts
+++ projects/howdy/src/lib/weather/howdy-weather.component.ts
@@ -2,7 +2,7 @@ import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'howdy-weather',
-  template: `<h1>Hello the weather is {{weather}}</h1>`
+  template: `<h1>The weather is {{weather}}</h1>`
 })
 export class HowdyWeatherComponent {
   @Input() weather: string;
