eda94aa8e3bc923ba3158020319de1113b94a6fa
kreuzerk <kevin.kreuzer90@icloud.com>
2019-09-02 10:25:05 +0200

    feat(inlinecss): illustrate working project with single bundle

--- projects/howdy/src/lib/weather/howdy-weather.component.ts
+++ projects/howdy/src/lib/weather/howdy-weather.component.ts
@@ -2,7 +2,11 @@ import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'howdy-weather',
-  template: `<h1>The weather is {{weather}}</h1>`
+  template: `
+      <h1>The weather is {{weather}}</h1>
+      <a href="https://weather.com/de-CH/wetter/heute/l/SZXX0006:1:SZ"></a>
+  `,
+  styleUrls: ['./howdy-weather.component.css']
 })
 export class HowdyWeatherComponent {
   @Input() weather: string;
